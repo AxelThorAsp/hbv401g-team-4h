d3b88636eaa61ac3507d2f55b99e5776f9885a74
AxelThorAsp <axel.aspelund@gmail.com>
2023-03-21 15:33:55 +0000

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.hbv401gteam4h {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.hbv401gteam4h to javafx.fxml;
+    exports com.example.hbv401gteam4h;
+}
