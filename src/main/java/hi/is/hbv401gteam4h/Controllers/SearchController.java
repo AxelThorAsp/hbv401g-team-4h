1247c0678e1b6c334ae67aa6a1919b98fbde3808
AxelThorAsp <axel.aspelund@gmail.com>
2023-03-26 22:44:37 +0000

    small change

--- src/main/java/hi/is/hbv401gteam4h/Controllers/SearchController.java
+++ src/main/java/hi/is/hbv401gteam4h/Controllers/SearchController.java
@@ -5,11 +5,12 @@ import hi.is.hbv401gteam4h.Persistence.Entities.HotelIndex;
 import hi.is.hbv401gteam4h.Persistence.Enums.HotelPriceEnum;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class SearchController {
     public static List<Hotel> searchHotels(String country, String city, HotelPriceEnum priceEnum) {
-        List<Hotel> result = new ArrayList<>();
+        List<Hotel> result = new LinkedList<>();
         for (Hotel h: HotelIndex.getHotelList()) {
             if (h.getCountry().equals(country) && h.getCity().equals(city) && h.getPrice().equals(priceEnum))
                 result.add(h);
@@ -17,7 +18,7 @@ public class SearchController {
         return result;
     }
     public static List<Hotel> searchHotels(String country) {
-        List<Hotel> result = new ArrayList<>();
+        List<Hotel> result = new LinkedList<>();
         for (Hotel h: HotelIndex.getHotelList()) {
             if (h.getCountry().equals(country))
                 result.add(h);
@@ -25,9 +26,17 @@ public class SearchController {
         return result;
     }
     public static List<Hotel> searchHotels(HotelPriceEnum priceEnum) {
-        List<Hotel> result = new ArrayList<>();
+        List<Hotel> result = new LinkedList<>();
         for (Hotel h: HotelIndex.getHotelList()) {
             if (h.getPrice().equals(priceEnum))
+                result.add(h);
+        }
+        return result;
+    }
+    public static List<Hotel> searchHotelName(String name) {
+        List<Hotel> result = new LinkedList<>();
+        for (Hotel h: HotelIndex.getHotelList()) {
+            if (h.getName().equals(name))
                 result.add(h);
         }
         return result;
